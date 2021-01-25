94c0e0b7f39feed11047fc8d4a6ccd5742ac0184
karol <karol.jarosinski91@gmail.com>
2021-01-25 15:06:48 +0100

    zadanie dodatkowe

--- src/Sort.java
+++ src/Sort.java
@@ -1,15 +1,21 @@
 public class Sort {
 
     public void sortArray(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
+        int counter = 0;
+        boolean isSwap = true;
+        for (int i = 0; i < array.length - 1 && isSwap; i++) {
+            isSwap = false;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                counter++;
                 if (array[j] > array[j + 1]) {
+                    isSwap = true;
                     int tmp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = tmp;
                 }
             }
         }
+        System.out.println("Liczba porownan " + counter);
     }
 
     public void printArray(int[] array) {
--- src/SortTest.java
+++ src/SortTest.java
@@ -1,8 +1,17 @@
 public class SortTest {
     public static void main(String[] args) {
-        int[] array = {4, 2, 5, 1, 7};
+//        int[] array = {4, 2, 5, 1, 7};
+//        int[] array = {1, 2, 3, 4, 5};
+        int[] array = {3, 2, 1};
+//        int[] array = new int[100];
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = 100 - i;
+//        }
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = i;
+//        }
         Sort sort = new Sort();
         sort.sortArray(array);
-        sort.printArray(array);
+//        sort.printArray(array);
     }
 }
